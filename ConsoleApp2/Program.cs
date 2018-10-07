60a6365e96be185fa4fbd2491c3cbf0bbda50b77
Alejandro Rodriguez <alejandro@dycoder.com>
2018-10-07 09:45:57 -0400

    Targeting frameworks

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using Flagger;
+using Flagger.Sources;
+using Flagger.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = new InMemoryFeatureSource(new[]
+            {
+                new Feature
+                {
+                    Name = "solution",
+                    Enabled = true,
+                    Strategies = new[]
+                    {
+                        new Strategy { Name = "a" },
+                        new Strategy { Name = "b", Enabled = true }
+                    }
+                }
+            });
+            Flag.SetSource(source);
+
+            var container = new UnityContainer();
+
+            container.RegisterType<ISolution>(new FeatureInjectionMember("solution",
+                                                                    new StrategyTypeResolver("a", typeof(SolutionA)),
+                                                                    new StrategyTypeResolver("b", typeof(SolutionB))));
+
+            var solution = container.Resolve<ISolution>();
+            Console.WriteLine(solution.Get());
+        }
+    }
+
+    public interface ISolution { string Get(); }
+    public class SolutionA : ISolution
+    {
+        public string Get()
+            => "A";
+    }
+    public class SolutionB : ISolution
+    {
+        public string Get()
+            => "B";
+    }
+}
