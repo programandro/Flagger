83f53b8d4eb32be6bca32571620b0fe67e1affef
Alejandro Rodriguez <alejandro@dycoder.com>
2018-10-07 10:13:27 -0400

    Strauggling with configuration

---
+++ Flagger/Sources/ConfigurationFeatureSource.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flagger.Sources
+{
+    public class ConfigurationFeatureSource : PreloadFeatureSource
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationFeatureSource(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public ConfigurationFeatureSource()
+        {
+            //new ConfigurationBuilder().AddXmlFile()
+        }
+
+        protected override IEnumerable<Feature> LoadFeatures()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
